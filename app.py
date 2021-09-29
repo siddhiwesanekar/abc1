cffe374d2ac621971f36bbfa74b50d22c10e412c
siddhiwesanekar <90743476+siddhiwesanekar@users.noreply.github.com>
2021-09-29 16:39:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/get1", methods=['GET'])
+@app.route("/get", methods=['GET'])
 def get_bot_response():
     userText = request.args.get('msg')
     msg5 = response(userText)
