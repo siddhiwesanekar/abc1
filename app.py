c3fcb4fd7307215e7e7a68d2eabeef04d28b09a4
siddhiwesanekar <90743476+siddhiwesanekar@users.noreply.github.com>
2021-09-29 14:52:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import threading
 from flask import Flask, render_template, request
 from test import response
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates')
 
 
 app.static_folder = './static'
